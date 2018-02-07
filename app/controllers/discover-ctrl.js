e53e0f0ed7b06dcbf179b80d083674fbeda83514
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-02-07 15:52:51 -0600

    added array shuffling function to give discover page it's discovery magic

--- app/controllers/discover-ctrl.js
+++ app/controllers/discover-ctrl.js
@@ -5,10 +5,19 @@ angular
   .controller('DiscoverCtrl', function($scope, BlogsFactory, $sanitize) {
     $scope.title = "Discover";
 
+    const shuffleArr = (array) => {
+      for (let i = array.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    };
+    
     BlogsFactory.getAllBlogs()
       .then(blogsArr => {
-          $scope.blogs = blogsArr;
-          console.log(blogsArr);
-      });
+        $scope.blogs = shuffleArr(blogsArr);
+        console.log(blogsArr);
+    });
+
 
   });
